4675d72e673ab3665e4d331404794bed14e808d3
John Hyde Menthula <111866563+johnhyde3@users.noreply.github.com>
2025-07-03 14:52:34 +0530

    Add files via upload

--- Assignment 5.py
+++ Assignment 5.py
@@ -1,11 +1,8 @@
 #1.   Creates a dictionary where student names are keys and their marks are values.
 #2.   Asks the user to input a student's name.
 #3.   Retrieves and displays the corresponding marks.
-#4.   If the student’s name is not found, display an appropriate message.
+#4.   If the student’s name is not found, display an appropriate message
 
-
-
-    # Step 1: Create a dictionary with student names and their marks
 students = {'john': 56,'hyde': 59,'menthula': 89}
 name = input("Enter the student's name: ")
 if name in students:
